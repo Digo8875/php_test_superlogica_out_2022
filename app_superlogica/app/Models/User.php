cdf68c19195d26e18c3c692ad47e0123f0236cc2
Rodrigo Cândido <rodrigo.candido@redevistorias.com.br>
2022-10-22 10:47:18 -0300

    refactor(model): Adjust User model fields

--- app_superlogica/app/Models/User.php
+++ app_superlogica/app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'fullName',
+        'userName',
+        'zipCode',
         'email',
         'password',
     ];
@@ -30,7 +32,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -38,7 +39,5 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = [];
 }
