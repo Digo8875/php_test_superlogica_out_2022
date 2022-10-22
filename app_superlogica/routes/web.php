c20bc2af5a092428ec8c50702b94e9aa7a51a42b
Rodrigo Cândido <rodrigo.candido@redevistorias.com.br>
2022-10-22 07:56:38 -0300

    fix(view): Adjust route name to root endpoint

--- app_superlogica/routes/web.php
+++ app_superlogica/routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home')->name('home');
-});
+    return view('home');
+})->name('home');
 
 #Exercício 1 - Formulário
 Route::get('/form', [App\Http\Controllers\FormExerciseController::class, 'form'])->name('form');
