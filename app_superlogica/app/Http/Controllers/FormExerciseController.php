9569a46062b3d62c292d2de924972a7ab2ec6e7d
Rodrigo Cândido <rodrigo.candido@redevistorias.com.br>
2022-10-23 14:15:53 -0300

    refactor(exercise_1): Improve the exercise 1 code logic

--- app_superlogica/app/Http/Controllers/FormExerciseController.php
+++ app_superlogica/app/Http/Controllers/FormExerciseController.php
@@ -3,21 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterPostRequest;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class FormExerciseController extends Controller
 {
+    /**
+    * View para exibição do formulário de cadastro de usuário do Exercício 1
+    */
     public function index()
     {
         $users = User::get();
         return view('exercise_1.index', ['users' => $users]);
     }
 
+    /**
+    * View para exibição do formulário de cadastro de usuário do Exercício 1
+    */
     public function form()
     {
         return view('exercise_1.form');
     }
 
+    /**
+    * Cadastrar usuário através do formulário recebido no request
+    * @param RegisterPostRequest $request parâmetros da requisição que contém
+    * campos do formulário para cadastro do usuário. É realizado a validação
+    * através da injeção de dependência do formRequest
+    */
     public function store(RegisterPostRequest $request)
     {
         $userData = $request->only(
@@ -27,6 +40,8 @@ class FormExerciseController extends Controller
             'email',
             'password'
         );
+
+        $userData['password'] = Hash::make($userData['password']);
 
         User::create($userData);
 
--- app_superlogica/resources/views/exercise_1/index.blade.php
+++ app_superlogica/resources/views/exercise_1/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base_view')
 
 @section('content')
-    <div class="col-12 m-0 p-0">
+    <div class="col-12 m-0 p-0 text-center">
         <div class="row m-0 p-4">
             <div class="col-12">
                 <a href="{{ route('home') }}" class="btn btn-primary m-1"> Home </a>
@@ -12,12 +12,35 @@
         <div class="row m-0 p-0 justify-content-center mt-3">
             @if($users->isEmpty())
                 <div>
-                    Não há usuários registrados no sistema
+                    <span class="border border-1 rounded p-2">
+                        Não há usuários registrados no sistema
+                    </span>
                 </div>
             @else
-                @foreach($users as $user)
-                    xxx
-                @endforeach
+                <div>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nome Completo</th>
+                                <th scope="col">User Name</th>
+                                <th scope="col">CEP</th>
+                                <th scope="col">Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                                <tr>
+                                    <th scope="row">{{ $loop->index + 1 }}</th>
+                                    <td>{{ $user->fullName }}</td>
+                                    <td>{{ $user->userName }}</td>
+                                    <td>{{ $user->zipCode }}</td>
+                                    <td>{{ $user->email }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             @endif
         </div>
     </div>
